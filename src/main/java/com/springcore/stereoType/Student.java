1981331430280d7736bc5f5ea278e939f958e183
ASUS <pialkhanpial@gmail.com>
2023-03-18 03:40:18 +0600

    second

--- src/main/java/com/springcore/stereoType/Student.java
+++ src/main/java/com/springcore/stereoType/Student.java
@@ -1,5 +1,7 @@
 package com.springcore.stereoType;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -10,21 +12,38 @@ class StudentClass {
 	
 	@Value("Dinajpur")
 	private String city;
+	
+	@Value("#{temp}")
+	private List<String> address;
+	
 	public String getStudentName() {
 		return studentName;
 	}
-	public void setStudentName(String studentName) {
-		this.studentName = studentName;
-	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
+	public List<String> getAddress() {
+		return address;
+	}
+
+	public void setAddress(List<String> address) {
+		this.address = address;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
 	@Override
 	public String toString() {
-		return "Student [studentName=" + studentName + ", city=" + city + "]";
+		return "StudentClass [studentName=" + studentName + ", city=" + city + ", address=" + address + "]";
 	}
+	 
 	
 }
