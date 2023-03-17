9f6865ce34ceb808f1fffecab35506c7f5b99790
ASUS <pialkhanpial@gmail.com>
2023-03-18 03:50:31 +0600

    second

--- src/main/java/com/springcore/stereoType/Test.java
+++ src/main/java/com/springcore/stereoType/Test.java
@@ -1,20 +1,25 @@
 package com.springcore.stereoType;
 
- 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
- 
 public class Test {
 
 	public static void main(String[] args) {
 
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/stereoType/cstereo.xml");
-		
-		StudentClass s = context.getBean("balbal",StudentClass.class) ;
-		
-		System.out.println(s); 
-	
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/springcore/stereoType/cstereo.xml");
+
+		StudentClass s = context.getBean("balbal", StudentClass.class);
+
+		System.out.println(s);
+		System.out.println(s.hashCode());
+
+		StudentClass s1 = context.getBean("balbal", StudentClass.class);
+
+		System.out.println(s1);
+		System.out.println(s1.hashCode());
+
 	}
 
 }
